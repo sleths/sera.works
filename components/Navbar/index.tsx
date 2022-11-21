2e69a44e3dd0ab03fe939da29683555893d2bd7b
Sera Zenginler <serazenginler@outlook.com>
2022-11-21 21:57:25 +0300

    drawer animation

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,61 +1,91 @@
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
-import LinesDuoTone from "../../assets/icons/lines.svg";
-import CloseDuoTone from "../../assets/icons/close.svg";
-import Dots from "../../assets/textures/dots.svg";
+import { useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+/* import Dots from "../../assets/textures/dots.svg"; */
 import styles from "./styles.module.scss";
+import useWindowSize from "../../hooks/useWindowSize";
 
 const Navbar = () => {
+  const size = useWindowSize();
+
   const [visible, setVisible] = useState(false);
 
   const ref = useRef<any>();
 
-  const openNav = () => setVisible(true);
-
   const closeNav = () => setVisible(false);
 
-  useEffect(() => {
-    if (ref.current === undefined || ref.current === null) return;
-    let handler = (event: any) => {
-      if (!ref!.current.contains(event.target)) {
-        closeNav();
-      }
-    };
-    document.addEventListener("mousedown", handler);
+  const animateProperty = size.width! < 1440 ? "height" : "width";
 
-    return () => {
-      document.removeEventListener("mousedown", handler);
-    };
-  });
+  const animatePercentage = size.width! < 1440 ? "100%" : "35%";
+
+  const transition = {
+    type: "spring",
+    stiffness: 85,
+    damping: 10,
+    mass: 1,
+    bounce: 0.25,
+  };
+
+  const variants = {
+    hidden: {
+      [animateProperty]: 0,
+      transition: transition,
+    },
+    visible: {
+      [animateProperty]: animatePercentage,
+      transition: transition,
+    },
+  };
+
+  const item = {
+    visible: { opacity: 1, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, transition: { duration: 0.3 } },
+  };
+
+  const toggle = () => setVisible(!visible);
 
   return (
-    <nav className={`center ${styles.nav}`}>
+    <nav className={`center ${styles.nav}`} onClick={toggle}>
       {/* Open Icon */}
-      <LinesDuoTone onClick={openNav} className={styles.openIcon} />
+      <ChevronLeftIcon
+        onClick={toggle}
+        className={`${styles.icon} ${
+          visible ? styles.closeIcon : styles.openIcon
+        }`}
+      />
 
-      {visible && (
-        <aside ref={ref}>
-          {/* Close Icon */}
-          <div className={`center ${styles.close}`} onClick={closeNav}>
-            <CloseDuoTone />
-          </div>
-          <ul>
-            <li onClick={closeNav}>
-              <Link href="/">Home</Link>
-            </li>
-            <li onClick={closeNav}>
-              <Link href="#about">About</Link>
-            </li>
-            {/* <li>
+      <AnimatePresence>
+        {visible && (
+          <motion.aside
+            ref={ref}
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+            exit={{
+              [animateProperty]: 0,
+            }}
+          >
+            {/* Close Icon */}
+
+            <motion.ul variants={item} exit={{ opacity: 0 }}>
+              <motion.li onClick={closeNav}>
+                <Link href="/">Home</Link>
+              </motion.li>
+              <motion.li onClick={closeNav}>
+                <Link href="#about">About</Link>
+              </motion.li>
+              {/* <li>
               <Link href="#work">Work</Link>
             </li> */}
-            <li onClick={closeNav}>
-              <Link href="#contact">Contact</Link>
-            </li>
-          </ul>
-          {/*  <Dots className={styles.dots} /> */}
-        </aside>
-      )}
+              <motion.li onClick={closeNav}>
+                <Link href="#contact">Contact</Link>
+              </motion.li>
+            </motion.ul>
+            {/*  <Dots className={styles.dots} /> */}
+          </motion.aside>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
